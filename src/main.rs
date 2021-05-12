7997316c54a60df9c2a4e71a9359141de11cb148
Dongze He <32473855+DongzeHE@users.noreply.github.com>
2021-05-12 19:29:39 -0400

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use bio::io::fasta;
+use bio::pattern_matching::myers::Myers;
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    // read in file
+    let fasta_file = std::path::Path::new(
+        "/Volumes/T7/combine/project/afvelo/readAnnotation/DATA/su_separated/gencode.vM25.annotation.expanded.unspliced.fa",
+    );
+    let mut out_file = File::create(
+        "/Volumes/T7/combine/project/afvelo/readAnnotation/DATA/su_separated/polyA2t.tsv",
+    )
+    .unwrap();
+    let reader = fasta::Reader::from_file(fasta_file).unwrap();
+
+    // set up probes
+    let mut poly_a = Myers::<u64>::new(vec![65u8; 31]);
+    let mut poly_t = Myers::<u64>::new(vec![84u8; 31]);
+
+    // iterate over records
+    for result in reader.records() {
+        let record = result.expect("Error during fasta record parsing");
+
+        // get matches
+        let mut poly_occ: Vec<_> = poly_a
+            .find_all(record.seq(), 1)
+            .map(|(a, _, _)| a as u64)
+            .collect();
+        let mut poly_t_occ: Vec<_> = poly_t
+            .find_all(record.seq(), 1)
+            .map(|(a, _, _)| a as u64)
+            .collect();
+        poly_occ.append(&mut poly_t_occ);
+        poly_occ.sort();
+        poly_occ.dedup();
+        // if no hit, next record
+        if poly_occ.is_empty() {
+            continue;
+        }
+
+        // if there are hits, process polyA then polyT
+        if poly_occ.len() > 1 {
+            // process the hit results in the format Vec<(start, end, edit distance)>
+            // a long polyA will produce many hits, so remove continuous hits
+            let mut keep = vec![true; poly_occ.len()];
+            for idx in 1..poly_occ.len() {
+                if poly_occ[idx] <= (poly_occ[idx - 1] + 31) {
+                    keep[idx] = false;
+                }
+            }
+            let mut i = 0;
+            poly_occ.retain(|_| (keep[i], i += 1).0);
+        }
+        // println!("{:?}", type_of(poly_t_occ.first()));
+        // writeln!(out_file, "{}\t{}\t{}", record.id(), poly_a_occ.len(), poly_a_occ.join("\t"))
+        writeln!(
+            &mut out_file,
+            "{}\t{}\t{}",
+            record.id(),
+            poly_occ.len(),
+            poly_occ
+                .into_iter()
+                .map(|a| a.to_string())
+                .collect::<Vec<String>>()
+                .join("\t")
+        )
+        .unwrap();
+    }
+}
